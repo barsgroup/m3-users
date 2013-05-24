09348797ef0aeec53557954d3c6b11e66e2979e9
Пирогов Алексей <devnull@localhost>
2013-05-24 15:49:37 +0400

    Fix имени пакета

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requires = []
 with open('src/requires.txt', 'r') as f:
     requires.extend(f.readlines())
 
-setup(name='m3_users',
+setup(name='m3-users',
       version='1.1.3',
       url='https://src.bars-open.ru/py/m3/m3_contrib/m3_users',
       license='Apache License, Version 2.0',
