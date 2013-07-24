575fcfbb30737ba7a26b6c05c4644f387559e794
Пирогов Алексей <devnull@localhost>
2013-07-24 08:22:56 +0400

    Теперь метароль - это только поле выбора (нередактируемое)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('src/requires.txt', 'r') as f:
     requires.extend(f.readlines())
 
 setup(name='m3-users',
-      version='1.1.4',
+      version='1.1.5',
       url='https://src.bars-open.ru/py/m3/m3_contrib/m3_users',
       license='Apache License, Version 2.0',
       author='BARS Group',
